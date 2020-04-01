aa8565b978f77e850b863df27d2ca08131ba42e0
sashaweiss <asashaweiss@gmail.com>
2020-03-31 19:56:44 -0700

    Add Xcode project

---
+++ portals/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  portals
+//
+//  Created by Sasha Weiss on 3/31/20.
+//  Copyright © 2020 natasha-codes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
